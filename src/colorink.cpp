6d17dff907786e33465be28b024137b3f83808a3
wittmaxi@outlook.de <wittmaxi@outlook.de>
2018-12-05 14:47:49 +0100

    Fixed small error

--- src/colorink.cpp
+++ src/colorink.cpp
@@ -46,7 +46,7 @@ namespace INK {
     boldOff_t boldOff;
 
     std::ostream& operator<< (std::ostream& os, const ColorMode& cm) {
-        std::cout << "\e";
+        std::cout << "\e[";
         if (cm.fg != 0xff)
             std::cout << cm.fg << ";";
         if (cm.bg != 0xff)
